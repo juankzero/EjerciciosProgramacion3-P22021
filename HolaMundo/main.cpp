1909bd7d7778f406afd031ccbff0b25100f915d3
juankzero <juan.zero@unitec.edu>
2021-04-21 10:57:09 -0600

    Add project files.

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <conio.h>
+
+using std::cout;
+
+int main() 
+{
+	cout << "Hola Mundo!\n";
+	cout << "Mi nombre es: " << "Juan Zepeda\n";
+
+	_getch();
+	return 0;
+}
